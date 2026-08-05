683e806e90d8baa5ff8db8019b52a6bd36ecff58
yinxuehao <1@qq.com>
2026-08-05 15:15:37 +0800

    set package com.luna.music, arm64-v8a only, signed release

---
+++ android/app/src/main/java/com/luna/music/MainApplication.java
@@ -0,0 +1,74 @@
+package com.luna.music;
+
+import com.facebook.react.PackageList;
+import com.facebook.react.flipper.ReactNativeFlipper;
+import com.reactnativenavigation.NavigationApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import java.util.List;
+
+import com.luna.music.cache.CachePackage;
+import com.luna.music.crypto.CryptoPackage;
+import com.luna.music.lyric.LyricPackage;
+import com.luna.music.userApi.UserApiPackage;
+import com.luna.music.utils.UtilsPackage;
+import com.luna.music.widget.WidgetPackage;
+
+public class MainApplication extends NavigationApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new NavigationReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          packages.add(new CachePackage());
+          packages.add(new LyricPackage());
+          packages.add(new UtilsPackage());
+          packages.add(new CryptoPackage());
+          packages.add(new UserApiPackage());
+          packages.add(new WidgetPackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+
+        @Override
+        protected Boolean isHermesEnabled() {
+          return BuildConfig.IS_HERMES_ENABLED;
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      DefaultNewArchitectureEntryPoint.load();
+    }
+    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+}
