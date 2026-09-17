45a2bb23d40a820388ee3a317d1b077aa34e7578
yinxuehao <1@qq.com>
2026-09-17 15:50:21 +0800

    fix: 同步上游 tx 搜索修复与自动换源匹配优化

--- src/utils/musicSdk/index.js
+++ src/utils/musicSdk/index.js
@@ -6,6 +6,7 @@ import mg from './mg'
 import git from './git'
 // import yt from './yt'
 import { supportQuality } from './api-source'
+import { versionChars } from './versionChars'
 
 const sources = {
   sources: [
@@ -127,8 +128,15 @@ export const findMusic = async (musicInfo) => {
   const fSinger = filterStr(sortSingle(singer)).toLowerCase()
   const fAlbumName = filterStr(albumName).toLowerCase()
   const fInterval = getIntv(interval)
-  const isEqualsInterval = (intv) => Math.abs((fInterval || intv) - (intv || fInterval)) < 5
-  const isIncludesName = (name) => fMusicName.includes(name) || name.includes(fMusicName)
+  const isEqualsInterval = (intv) => Math.abs((fInterval || intv) - (intv || fInterval)) <= 5
+  const isEqualsVersionMusicNameChar = (name) => {
+    for (const char of versionChars) {
+      if (name.includes(char) != fMusicName.includes(char)) return false
+    }
+    return true
+  }
+  const isIncludesName = (name) =>
+    (fMusicName.includes(name) || name.includes(fMusicName)) && isEqualsVersionMusicNameChar(name)
   const isIncludesSinger = (singer) =>
     fSinger ? fSinger.includes(singer) || singer.includes(fSinger) : true
   const isEqualsAlbum = (album) => (fAlbumName ? fAlbumName == album : true)
